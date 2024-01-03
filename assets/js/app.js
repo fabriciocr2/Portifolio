b58dd3f7bb9f73e6119d24dcb3e2c81a2865ce22
fabriciocr <fabriciocr1980@hotmail.com>
2024-01-02 22:34:25 -0300

    segundo commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,61 @@
 window.revelar = ScrollReveal({ reset: true });
 
 revelar.reveal(".inicio", {
-  duration: 1000,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "left",
 });
 
 revelar.reveal(".inicio2", {
-  duration: 1000,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "right",
 });
 
 revelar.reveal(".sobre", {
-  duration: 1500,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "bottom",
 });
 
 revelar.reveal(".sobre2", {
-  duration: 1500,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "top",
 });
 revelar.reveal(".Title", {
-  duration: 1500,
+  duration: 900,
   distance: "1,rem",
   delay: 500,
   origin: "bottom",
 });
 
 revelar.reveal(".skills1", {
-  duration: 1500,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "left",
 });
 
 revelar.reveal(".skills2", {
-  duration: 1500,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "bottom",
 });
 
 revelar.reveal(".skills3", {
-  duration: 1500,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "right",
 });
 revelar.reveal(".title-projects", {
-  duration: 1500,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "right",
@@ -63,13 +63,13 @@ revelar.reveal(".title-projects", {
 
 
 revelar.reveal(".contato1", {
-  duration: 1500,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "top",
 });
 revelar.reveal(".contato2", {
-  duration: 1500,
+  duration: 900,
   distance: "10rem",
   delay: 500,
   origin: "bottom",
