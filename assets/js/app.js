90457736093776442ae5c13ceb7825afa988d0c4
fabriciocr <fabriciocr1980@hotmail.com>
2024-01-02 21:42:49 -0300

    Primeiro commit

---
+++ assets/js/app.js
@@ -0,0 +1,76 @@
+window.revelar = ScrollReveal({ reset: true });
+
+revelar.reveal(".inicio", {
+  duration: 1000,
+  distance: "10rem",
+  delay: 500,
+  origin: "left",
+});
+
+revelar.reveal(".inicio2", {
+  duration: 1000,
+  distance: "10rem",
+  delay: 500,
+  origin: "right",
+});
+
+revelar.reveal(".sobre", {
+  duration: 1500,
+  distance: "10rem",
+  delay: 500,
+  origin: "bottom",
+});
+
+revelar.reveal(".sobre2", {
+  duration: 1500,
+  distance: "10rem",
+  delay: 500,
+  origin: "top",
+});
+revelar.reveal(".Title", {
+  duration: 1500,
+  distance: "1,rem",
+  delay: 500,
+  origin: "bottom",
+});
+
+revelar.reveal(".skills1", {
+  duration: 1500,
+  distance: "10rem",
+  delay: 500,
+  origin: "left",
+});
+
+revelar.reveal(".skills2", {
+  duration: 1500,
+  distance: "10rem",
+  delay: 500,
+  origin: "bottom",
+});
+
+revelar.reveal(".skills3", {
+  duration: 1500,
+  distance: "10rem",
+  delay: 500,
+  origin: "right",
+});
+revelar.reveal(".title-projects", {
+  duration: 1500,
+  distance: "10rem",
+  delay: 500,
+  origin: "right",
+});
+
+
+revelar.reveal(".contato1", {
+  duration: 1500,
+  distance: "10rem",
+  delay: 500,
+  origin: "top",
+});
+revelar.reveal(".contato2", {
+  duration: 1500,
+  distance: "10rem",
+  delay: 500,
+  origin: "bottom",
+});
